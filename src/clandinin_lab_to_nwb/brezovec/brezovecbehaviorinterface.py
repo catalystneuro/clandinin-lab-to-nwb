028ab5a61488937439823f06203a87ac450e56f9
Heberto Mayorquin <h.mayorquin@gmail.com>
2023-07-13 10:04:42 +0200

    add keyword

--- src/clandinin_lab_to_nwb/brezovec/brezovecbehaviorinterface.py
+++ src/clandinin_lab_to_nwb/brezovec/brezovecbehaviorinterface.py
@@ -2,21 +2,24 @@
 from pynwb.file import NWBFile
 
 from neuroconv.basedatainterface import BaseDataInterface
+from neuroconv.utils import DeepDict
 
 class BrezovecBehaviorInterface(BaseDataInterface):
     """Behavior interface for brezovec conversion"""
+
+    keywords = ["fictrack", "visual tracking", "fictive path", "spherical treadmill", "visual fixation",]
 
     def __init__(self):
         # This should load the data lazily and prepare variables you need
         pass
 
-    def get_metadata(self):
+    def get_metadata(self) -> DeepDict:
         # Automatically retrieve as much metadata as possible
         metadata = super().get_metadata()
 
         return metadata
 
-    def add_to_nwb(self, nwbfile: NWBFile, metadata: dict):
+    def add_to_nwbfile(self, nwbfile: NWBFile, metadata: dict):
         # All the custom code to write to PyNWB
 
         return nwbfile
