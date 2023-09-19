ac4988341f7f387e5862377f74e2a5e7f8791ee6
Heberto Mayorquin <h.mayorquin@gmail.com>
2023-09-19 08:00:02 +0200

    add videointerface

--- src/clandinin_lab_to_nwb/brezovec/brezovec_convert_session.py
+++ src/clandinin_lab_to_nwb/brezovec/brezovec_convert_session.py
@@ -16,22 +16,34 @@ def session_to_nwb(data_dir_path: Union[str, Path], output_dir_path: Union[str, 
         output_dir_path = output_dir_path / "nwb_stub"
     output_dir_path.mkdir(parents=True, exist_ok=True)
 
-    session_id = "subject_identifier_usually"
+    session_id = "20200228_161226"
+
+    # Parse subject_id and session_id
+    unformated_date, subject_id = session_id.split("_")  # TODO: maybe enchance subject id this with the fly number
+    parsed_date = datetime.datetime.strptime(unformated_date, "%Y%m%d")
     nwbfile_path = output_dir_path / f"{session_id}.nwb"
 
     source_data = dict()
     conversion_options = dict()
 
     # Add Fictrac
-    file_path = data_dir_path / "fictrac" / "fictrac-20200228_161226.dat"
+    file_path = data_dir_path / "fictrac" / f"fictrac-{session_id}.dat"
     source_data.update(dict(FicTrac=dict(file_path=str(file_path))))
+
+    # Video
+    file_path = data_dir_path / "fictrac" / f"fictrac-{session_id}-raw.avi"
+    file_paths = [file_path]
+    source_data.update(dict(Video=dict(file_paths=file_paths)))
+    conversion_options.update(dict(Video=dict(stub_test=stub_test)))
 
     converter = BrezovecNWBConverter(source_data=source_data)
 
     # Add datetime to conversion
     metadata = converter.get_metadata()
-    date = datetime.datetime(year=2020, month=1, day=1, tzinfo=ZoneInfo("US/Eastern"))
+    tzinfo = ZoneInfo("America/Los_Angeles")  # Time zone for Stanford, California
+    date = parsed_date.replace(tzinfo=tzinfo)
     metadata["NWBFile"]["session_start_time"] = date
+    metadata["Subject"]["subject_id"] = subject_id
 
     # Update default metadata with the editable in the corresponding yaml file
     editable_metadata_path = Path(__file__).parent / "brezovec_metadata.yaml"
@@ -52,7 +64,7 @@ if __name__ == "__main__":
     root_path = Path("/home/heberto/Clandinin-CN-data-share/")
     data_dir_path = root_path / "brezovec_example_data"
     output_dir_path = Path("/home/heberto/conversion_nwb/")
-    stub_test = False
+    stub_test = True
 
     session_to_nwb(
         data_dir_path=data_dir_path,
