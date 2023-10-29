ddf4e0b32d2354a078ef5b47094931f96766b823
Heberto Mayorquin <h.mayorquin@gmail.com>
2023-10-29 13:11:01 +0100

    add camera

--- src/clandinin_lab_to_nwb/brezovec/brezovecnwbconverter.py
+++ src/clandinin_lab_to_nwb/brezovec/brezovecnwbconverter.py
@@ -1,4 +1,8 @@
 """Primary NWBConverter class for this dataset."""
+from typing import Dict, List, Optional, Union
+
+from pynwb import NWBFile
+
 from neuroconv import NWBConverter
 from neuroconv.datainterfaces import (
     FicTracDataInterface,
@@ -49,3 +53,16 @@ class BrezovecNWBConverter(NWBConverter):
         UTC_series_start_time = series_start_time.timestamp()
         aligned_starting_time = UTC_series_start_time - UTC_session_start_time
         anatomical_red_interface.set_aligned_starting_time(aligned_starting_time)
+
+    def add_to_nwbfile(self, nwbfile: NWBFile, metadata, conversion_options: Optional[dict] = None) -> None:
+        super().add_to_nwbfile(nwbfile, metadata, conversion_options=conversion_options)
+
+        # Add the camera
+        from pynwb.device import Device
+
+        name = "Flea FL3-U3-13E4M-C"
+        description = "Sensor used for imaging at 50 Hz with Edmund Optics 100 mm C Series Fixed Focal Length Lens"
+        manufacturer = "Teledyne FLIR Systems, Inc."
+        camera_device = Device(name, description=description, manufacturer=manufacturer)
+
+        nwbfile.add_device(camera_device)
